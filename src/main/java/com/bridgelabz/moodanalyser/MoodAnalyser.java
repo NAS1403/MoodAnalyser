45ff2e5a0e940c37d86ffec9a098081ea218ef4d
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-08-05 17:51:09 +0530

    Custom exception

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,33 +1,25 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
-
-    String message;
-
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String happyMood = moodAnalyser.analyseMood();
-        System.out.println("My mood is :"+happyMood);
-        String sadMood = moodAnalyser.analyseMood();
-        System.out.println("My Mood is :"+sadMood);
+    public enum MoodAnalysr{
+        NULL
     }
+    String message;
 
     public MoodAnalyser(String message) {
         this.message = message;
     }
 
-    public MoodAnalyser() {
-    }
-
-    public String analyseMood (){
+    public String analyseMood () throws MoodAnalyserException {
         try {
             if (message.toLowerCase().contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
         }
-        catch (Exception e){
-            return "HAPPY";
+        catch (NullPointerException e){
+            throw new MoodAnalyserException(e.getMessage());
+
         }
     }
 }
